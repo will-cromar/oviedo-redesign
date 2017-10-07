b39c8daaedcd9c453679a8267778563ff76e3cfb
will-cromar <wcromar@knights.ucf.edu>
2017-10-07 15:51:33 -0400

    Finished adding links to the navigational elements

--- app/views.py
+++ app/views.py
@@ -1,45 +1,101 @@
 from flask import render_template
 from app import app
 
+# Links that still need a home:
+# Oviedo on the Park
+# Contact Us
+# E-mail news sign-up
+
 sidebar_links = [
-    {"title": "Information",
-     "sublinks": [
-         "Events",
-         "Oviedo on the Park",
-         "Community Redevelopment Agency Documents",
-         "Oviedo Veterans Tribute",
-         "State of the City",
-         "Publications and Reports",
-         "City Strategic Plan",
-         "Resident Information",
-         "Street Light Outage, Solid Waste, Water Conservation, Flood Zone Info",
-         "Utility Emergency Numbers and Information",
-         "Frequently Asked Questions",
-         "For Business",
-         "Public Notices",
-         "Accessibility",
-     ]},
-    {"title": "Online Services",
-     "sublinks": [
-         "Citizen Request",
-         "City GIS Map Gallery",
-         "Friends of Oviedo",
-         "Find a Job",
-         "Pay your Utility Bill",
-         "Recreation Registration",
-         "Building Permits",
-         "Business Tax Receipts",
-         "Planning/Development Review"
-     ]}
+    ("Events", [
+        "Events Calendar",
+        "Movie in the Park Series",
+        "Food Truck Thursdays",
+        "Great Day in the Country",
+        "Halloween Events",
+        "Carnival of Screams",
+        "Teenie Weenei Haloween",
+        "12 Days of Christmas",
+        "Snow Mountain",
+        "Santa Around Town",
+        "Breakfast with Santa",
+        "Santa Calling"
+    ]),
+    ("Utilities", [
+        "Pay Your Utility Bill",
+        "High Water Usage?",
+        "Utility Emergency Information",
+        "Property Assessed Clean Energy Program",
+    ]),
+    ("Public Works", [
+        "Public Works Home",
+        "Water Conservation",
+
+    ])
 ]
 topbar_links = [
-    "Home",
-    "About Oviedo",
-    "City Departments",
-    "Government",
-    "Employment",
-    "Facilities",
-    "Getting Involved"
+    ("Home", None),
+    ("Information", [
+        "About Oviedo",
+        "History",
+        "Demographics",
+        "Frequently Asked Questions",
+        "Garbage FAQs",
+        "Irrigation Schedules",
+        "Senior Safety Program",
+        "Resident Information",
+        "Accessibility",
+        "For Business",
+        "New Resident Information",
+        "Sister City Information",
+        "Publications and Reports",
+    ]),
+    ("City Departments", [
+        "City Manager",
+        "Assistant City Manager",
+        "Development Services",
+        "Finance Department",
+        "Informtaion Technology",
+        "Fire Department",
+        "Human Resources",
+        "Public Works",
+        "Police",
+        "Recreation and Parks",
+    ]),
+    ("Government", [
+        "Government Home",
+        "Mayor and Council",
+        "City Clerk",
+        "Charter and Code of Ordinances",
+        "State of the City",
+        "City Stragegic Plan",
+        "Meetings List",
+        "Speak at Council Meeting",
+        "Citizen Request",
+    ]),
+    ("Facilities", [
+        "Public Facilities List",
+        "Cultural Center and Ampitheatre Information",
+        "Center Lake Park",
+        "Reporting Street Light Outages"
+    ]),
+    ("Development Services", [
+        "Development Services Home",
+        "Building Services",
+        "Planning and Development",
+        "Code Enforcement",
+        "Purchasing",
+        "Building Permits",
+        "Business Tax Receipt",
+        "Economic Development",
+        "Community Redevelopment Agency Documents",
+    ]),
+    ("Getting Involved", [
+        "Friends of Oviedo",
+        "Volunteer Opportunities",
+        "Employment",
+        "Flood Zone Map",
+    ]),
 ]
 
 settings = {
